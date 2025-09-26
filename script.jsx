1a4e028b228382adc345025177ad8208f128a2fb
Alorca <aa.lorca@duocuc.cl>
2025-09-26 20:27:36 -0300

    v.1.0.26 Reset Web Site

---
+++ script.jsx
@@ -0,0 +1,15 @@
+function Raiz() {
+  return (
+    <>
+      <h1>Vivencias, Aventuras, Inicios y Finales.</h1>
+      <hr />
+      <h2>Libros</h2>
+      <h3>Juegos</h3>
+    </>
+  );
+}
+
+const app = document.getElementById("app");
+const root = ReactDOM.createRoot(app);
+root.render(<Header />)
+;
